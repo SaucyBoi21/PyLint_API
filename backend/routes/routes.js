c4f568b24b48d05ae98f8f5982a296375e25bbcd
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-09-16 12:10:20 -0400

    added more dependencies and scripts to package.json

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,11 +1,11 @@
 const {upload} = require("../multer/multer_config")
-const npm = require('npm-commands') 
+const {exec} = require('child_process')
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 
 
 module.exports = router
-
 
 router.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
@@ -17,11 +17,30 @@ router.post('/upload', upload.single('file'), (req, res) => {
     })
 })
 
-router.get('/file', (req, res) => {
+router.get('/output', (req, res) => {
     data = req.body
     filename = data.filename
-    res.json({
-        "filename": filename
-        
+
+    pylintCommand = `npm exec npm`
+
+    exec(pylintCommand, (error, stdout, stderr) => {
+        if (error) {
+            res.status(500).json({
+                error: `Error: ${error.message}`
+            })
+        }
+
+        if (stderr) {
+            res.status(400).json({
+                error: 'No file found'
+            })
+        }
+
+        var output = stdout;
+
+        res.json({
+        "filename": filename,
+        "output": output
+        })
     })
 })
